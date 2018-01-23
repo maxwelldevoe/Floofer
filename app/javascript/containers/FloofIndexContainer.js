fe38c3ed93f46b01182e1adc32f7adae790a8a99
charrhia <charlotte.rhia.smith@gmail.com>
2018-01-23 11:27:30 -0500

    Router + First NavBar Story

---
+++ app/javascript/containers/FloofIndexContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FloofIndexContainer = (props) => {
+
+return(
+  <h1>Hello from Floof Index container</h1>
+)}
+
+export default FloofIndexContainer;
---
+++ app/javascript/containers/FloofShowContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FloofShowContainer = (props) => {
+
+  return(
+    <h1>Hello from Floof Show container</h1>
+  )}
+
+export default FloofShowContainer;
