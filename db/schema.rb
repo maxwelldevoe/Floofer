d6767a2dcb9ff0c4414d76adc6f989b274a3d096
Yuliya Zhak <yulia@Yuliyas-MacBook-Air.local>
2018-01-24 15:06:27 -0500

    fixed migrations/schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,16 @@ ActiveRecord::Schema.define(version: 20180124144436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "floofs", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "job_title", null: false
+    t.string "current_employer"
+    t.string "category", null: false
+    t.string "species", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
