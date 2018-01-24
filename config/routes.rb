a21f071145326b733984b906d4d8e3bc45160717
charrhia <charlotte.rhia.smith@gmail.com>
2018-01-24 10:36:39 -0500

    created API endpoint for floof database

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,12 @@ Rails.application.routes.draw do
   get '/floofs', to: 'homes#index'
   get '/floofs/:id', to: 'homes#index'
   get '/floofs/new', to: 'homes#index'
+
+
+    namespace :api do
+      namespace :v1 do
+        resources :floofs, only: [:index]
+      end
+    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
