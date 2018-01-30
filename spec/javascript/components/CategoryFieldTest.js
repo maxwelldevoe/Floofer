f02b58a223ac0ea3d7f5ecac0ce8f21862ee7952
Yuliya Zhak <yulia@Yuliyas-MacBook-Air.local>
2018-01-30 16:13:08 -0500

    Added New Floof Form

--- spec/javascript/components/CategoryFieldTest.js
+++ spec/javascript/components/CategoryFieldTest.js
@@ -1,16 +1,48 @@
 import CategoryField from '../../../app/javascript/components/CategoryField'
 
-describe('', () => {
+describe('Category Field', () => {
   let handleInput,
-  wrapper
+  wrapper,
+  mockCategoryData
 
   beforeEach(() => {
+    mockCategoryData = [{
+      id: 1,
+      name: 'Business'
+    },
+    {
+      id: 2,
+      name: 'Healthcare'
+    },
+    {
+      id: 3,
+      name: 'Constructions'
+    },
+    {
+      id: 4,
+      name: 'Technology'
+    },
+    {
+      id: 5,
+      name: 'Law'
+    },
+    {
+      id: 6,
+      name: 'Entertainment'
+    },
+    {
+      id: 7,
+      name: 'Government'
+    }
+  ]
+
     handleInput = jasmine.createSpy('handleInput.spy')
     wrapper = mount(
       <CategoryField
         label='Category'
         value='what state passes down'
         handleChange={ handleInput }
+        categories={ mockCategoryData }
       />
     )
   })
@@ -21,13 +53,13 @@ describe('', () => {
 
   it('should have an option tag with the proper value', () => {
     expect(wrapper.find('option').at(0).props().value).toEqual('')
-    expect(wrapper.find('option').at(1).props().value).toEqual('Business')
-    expect(wrapper.find('option').at(2).props().value).toEqual('Construction')
-    expect(wrapper.find('option').at(3).props().value).toEqual('Entertainment')
-    expect(wrapper.find('option').at(4).props().value).toEqual('Government')
-    expect(wrapper.find('option').at(5).props().value).toEqual('Healthcare')
-    expect(wrapper.find('option').at(6).props().value).toEqual('Law')
-    expect(wrapper.find('option').at(7).props().value).toEqual('Technology')
+    expect(wrapper.find('option').at(1).props().value).toEqual(1)
+    expect(wrapper.find('option').at(2).props().value).toEqual(2)
+    expect(wrapper.find('option').at(3).props().value).toEqual(3)
+    expect(wrapper.find('option').at(4).props().value).toEqual(4)
+    expect(wrapper.find('option').at(5).props().value).toEqual(5)
+    expect(wrapper.find('option').at(6).props().value).toEqual(6)
+    expect(wrapper.find('option').at(7).props().value).toEqual(7)
   })
 
   it('should invoke the handleput function from props input is provided', () => {
@@ -35,7 +67,3 @@ describe('', () => {
     expect(handleInput).toHaveBeenCalled()
   })
 })
-
-// {
-//   value: 'what state passes down',
-// }
