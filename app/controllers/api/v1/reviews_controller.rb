652da019ff5569a0896a5ac4b1e0bdf27d03d9e3
Andrew <apepperanderson@gmail.com>
2018-02-01 13:13:31 -0500

    delete review + testing

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -16,6 +16,12 @@ class Api::V1::ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    review = Review.find(params[:id])
+    review.destroy
+    render json: review
+  end
+
   private
 
     def review_params
